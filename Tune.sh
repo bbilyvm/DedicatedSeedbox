6974668f45972ce42fa540e1a9b5abd2bbc361fd
bbilyvm <82843124+bbilyvm@users.noreply.github.com>
2026-01-11 00:48:58 +0800

    Validate qBittorrent binary before install

--- Tune.sh
+++ Tune.sh
@@ -3,7 +3,7 @@
 tput sgr0; clear
 
 ## Load text color settings
-source <(wget -qO- https://raw.githubusercontent.com/i-abc/Seedbox-Components/main/Miscellaneous/tput.sh)
+source <(wget -qO- https://raw.githubusercontent.com/bbilyvm/DedicatedSeedbox/main/Miscellaneous/tput.sh)
 
 ## Check Root Privilege
 if [ $(id -u) -ne 0 ]; then 
@@ -13,13 +13,13 @@ fi
 
 ## Check Linux Distro
 distro_codename="$(source /etc/os-release && printf "%s" "${VERSION_CODENAME}")"
-if [[ $distro_codename != buster ]] && [[ $distro_codename != bullseye ]] ; then
-	warn_1; echo "Only Debian 10/11 is supported"; normal_4
+if [[ $distro_codename != buster ]] && [[ $distro_codename != bullseye ]] && [[ $distro_codename != bookworm ]] && [[ $distro_codename != trixie ]] ; then
+	warn_1; echo "Only Debian 10/11/12/13 is supported"; normal_4
 	exit 1
 fi
 
 while true; do
-    source <(wget -qO- https://raw.githubusercontent.com/i-abc/Seedbox-Components/main/tweaking.sh)
+    source <(wget -qO- https://raw.githubusercontent.com/bbilyvm/DedicatedSeedbox/main/tweaking.sh)
     normal_3; options=("Deluge Tuning" "Tweaked BBR Install" "System Tuning" "Configure Boot Script")
     select opt in "${options[@]}"
     do
@@ -39,7 +39,7 @@ while true; do
                 CPU_Tweaking; NIC_Tweaking; Network_Other_Tweaking; Scheduler_Tweaking; kernel_Tweaking; break
                 ;;
             "Configure Boot Script")
-                source <(wget -qO- https://raw.githubusercontent.com/i-abc/Seedbox-Components/main/Miscellaneous/boot-script.sh)
+                source <(wget -qO- https://raw.githubusercontent.com/bbilyvm/DedicatedSeedbox/main/Miscellaneous/boot-script.sh)
                 boot_script; break
                 ;;
             *) warn_1; echo "Please choose a valid action";;
